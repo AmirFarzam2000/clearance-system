7dcff8bd99c7ca50ea7b9f1949bd4cafd11c65f1
AmirFarzam2000 <amirmehdifarzamfar1380@gmail.com>
2025-10-26 11:33:34 +0330

    Add components index file for exports

---
+++ src/components/index.ts
@@ -0,0 +1,30 @@
+export { default as Logo } from './ui/Logo';
+export { default as Button } from './ui/Button';
+export { Modal, ConfirmModal } from './ui/Modal';
+export { SuccessModal } from './ui/SuccessModal';
+export { LoadingModal } from './ui/LoadingModal';
+export { default as TariffModal } from './ui/TariffModal';
+export { default as TariffL2Modal } from './ui/TariffL2Modal';
+export { default as SelectList } from './ui/SelectList';
+export { default as SelectField } from './ui/SelectField';
+export { default as ErrorModal } from './ui/ErrorModal';
+
+export { default as UserIcon } from './ui/icons/UserIcon';
+export { default as LockIcon } from './ui/icons/LockIcon';
+
+export { default as InputField } from './forms/InputField';
+export { default as LoginForm } from './forms/LoginForm';
+export { default as NewCurrencyForm } from './forms/NewCurrencyForm';
+export { default as EditCurrencyForm } from './forms/EditCurrencyForm';
+export { default as NewProductGroupForm } from './forms/NewProductGroupForm';
+export { default as FormInput } from './forms/FormInput';
+export { default as FormSelect } from './forms/FormSelect';
+export { default as CustomSelect } from './forms/CustomSelect';
+export { default as FormTextarea } from './forms/FormTextarea';
+export { default as FormButtons } from './forms/FormButtons';
+
+export { default as CurrencyManagement } from './pages/CurrencyManagement';
+export { default as TarrifsManagement } from './pages/TarrifsManagement';
+
+export { default as LoginIllustration } from './layout/LoginIllustration';
+export { default as LoginPage } from './layout/LoginPage';
